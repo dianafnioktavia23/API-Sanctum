d1d47a926741299483719185e7844f11f46bcf0d
dianafnioktavia23 <113124849+dianafnioktavia23@users.noreply.github.com>
2024-04-29 10:01:51 +0700

    update API

--- API-Sanctum/app/Http/Controllers/PemesananController.php
+++ API-Sanctum/app/Http/Controllers/PemesananController.php
@@ -51,38 +51,4 @@ class PemesananController extends Controller
       return response()->json(PemesananResource::collection($pemesanan));
   }
 
-//   public function store(Request $request)
-// {
-//     // Validasi request
-//     $validatedData = $request->validate([
-//         'nama_pengunjung' => 'required|string',
-//         'meja_id' => 'required|exists:meja,id',
-//         'tanggal_pemesanan' => 'required|date',
-//         'menus' => 'required|array',
-//         'menus.*.menu_id' => 'required|exists:menu,id',
-//         'menus.*.jumlah' => 'required|integer|min:1',
-//     ]);
-
-//     // Buat pemesanan baru
-//     $pemesanan = Pemesanan::create([
-//         'nama_pengunjung' => $validatedData['nama_pengunjung'],
-//         'meja_id' => $validatedData['meja_id'],
-//         'tanggal_pemesanan' => $validatedData['tanggal_pemesanan'],
-//         'status' => 'Pending', // Atur status sesuai kebutuhan
-//         'keterangan' => '', // Atur keterangan sesuai kebutuhan
-//     ]);
-
-//     // Proses setiap menu yang dipesan
-//     foreach ($validatedData['menus'] as $menu) {
-//         // Buat detail pemesanan untuk setiap menu
-//         detailpesanan::create([
-//             'pesanan_id' => $pemesanan->id,
-//             'menu_id' => $menu['menu_id'],
-//             'jumlah' => $menu['jumlah'],
-//             'subtotal' => $menu['jumlah'] * Menu::findOrFail($menu['menu_id'])->harga,
-//         ]);
-//     }
-
-//     return response()->json(['message' => 'Pemesanan berhasil', 'data' => $pemesanan], 201);
-// }
 }
